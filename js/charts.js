d84065792d0b5f527ae7ae0a11e496283dcf348d
Horace He <horacehe2007@yahoo.com>
2017-12-03 01:45:30 -0500

    First commit

---
+++ js/charts.js
@@ -0,0 +1,66 @@
+var request = new XMLHttpRequest();
+request.open('GET', '../public/data.json', false);
+request.send(null);
+var data = JSON.parse(request.responseText);
+
+let x = [];
+for (let paper of data) {
+  x.push(parseFloat(paper['rating']));
+}
+var trace = {
+  x: x,
+  type: 'histogram',
+  autobinx: false,
+  xbins: {
+    end: 10.0,
+    start: 0.0,
+    size: 0.5,
+  },
+};
+var data = [trace];
+let layout = {
+  title: 'Paper Average Score Distribution',
+};
+Plotly.newPlot('histogram', data, layout);
+
+var trace = {
+  x: x,
+  type: 'histogram',
+  histnorm: 'probability',
+  cumulative: { enabled: true },
+  autobinx: false,
+  xbins: {
+    end: 10.0,
+    start: 0.0,
+    size: 0.01,
+  },
+};
+let cumdata = [trace];
+let cumlayout = {
+  title: 'Paper Cumulative Score Distribution',
+  shapes: [
+    {
+      type: 'line',
+      x0: 0,
+      x1: 10,
+      y0: 0.5,
+      y1: 0.5,
+      line: {
+        color: 'rgb(255,165,0)',
+        width: 3,
+      },
+    },
+    {
+      type: 'line',
+      x0: 0,
+      x1: 10,
+      y0: 0.7,
+      y1: 0.7,
+      line: {
+        color: 'rgb(173,255,47)',
+        width: 3,
+      },
+    },
+  ],
+};
+Plotly.newPlot('cumulative', cumdata, cumlayout);
---
+++ js/index.js
@@ -0,0 +1,77 @@
+
+var options = {
+  valueNames: ['rank', 'rating', 'title', 'ratings', 'confidences', 'extra'],
+  item: `<tr><td class="rank"></td><td class="rating" ></td><td class="title"></td><td class="ratings"></td><td class="confidences"></td><td class="extra break" onclick="toggleAbstract(this)"></td></tr>`
+};
+var request = new XMLHttpRequest();
+request.open('GET', '../public/data.json', false);
+request.send(null);
+var data = JSON.parse(request.responseText);
+
+for (let i=0; i<data.length; i++) {
+    data[i]['title'] = `<a href=${data[i]['url']}>${data[i]['title']}</a>`;
+    data[i]['oldtitle'] = data[i]['title'];
+    data[i]['extra'] = `<i class="fa fa-chevron-up"></i>`;
+}
+let list = new List('users', options, data);
+list.sort('rating', { order: 'desc' });
+function updateHighlights() {
+    let searchString = document.getElementById('search').value.toLowerCase();
+    if (searchString.length < 3) {
+        searchString='akfdjhlhsajlshkfdajkhlsajfsahlkfdjldsajhkfkajhlfdshjl';
+    }
+    let items = list.matchingItems;
+    console.log(searchString);
+    let strings = [searchString, searchString[0].toUpperCase() + searchString.slice(1), searchString.toUpperCase()];
+    let highlightInString = (str) => {
+        for (let s of strings) {
+            str = str.replace(s, `<span class="highlight">${s}</span>`);
+        }
+        return str;
+    }
+    for (const item of items) {
+        let newValues = item._values;
+        newValues.title = highlightInString(newValues.oldtitle);
+        item.values(newValues);
+    }
+    let table = document.querySelectorAll('#table > tbody > tr');
+    for (let tr of table) {
+        if (tr.children.length === 1) {
+            tr.children[0].innerHTML = highlightInString(tr.children[0].innerHTML);
+        }
+    }
+}
+list.on('searchComplete', (e) => {
+    updateHighlights();
+})
+
+// function closeAbstract(rank) {
+//     let paper = list.get("rank", rank)[0];
+//     let curValues = paper._values;
+//     curValues.extra = `<span onclick="expandAbstract(${curValues['rank']})"><i class="fa fa-expand" aria-hidden="true"></i></span>`;
+//     curValues.abstract;
+//     paper.values(curValues);
+// }
+// function expandAbstract(rank) {
+//     let paper = list.get("rank", rank)[0];
+//     let curValues = paper._values;
+//     curValues.extra = `<span onclick="closeAbstract(${curValues['rank']})">${curValues.abstract}"</span>`;
+//     paper.values(curValues);
+// }
+
+function toggleAbstract(x) {
+    let rank = x.parentNode.getElementsByClassName('rank')[0].innerText;
+    let paper = list.get("rank", rank)[0];
+    let curValues = paper._values;
+    if (curValues.extra.indexOf('fa-chevron-up') === -1) {
+        curValues.extra = `<i class="fa fa-chevron-up" aria-hidden="true"></i>`;
+        x.parentNode.nextSibling.remove();
+    } else {
+        curValues.extra = `<i class="fa fa-chevron-down" aria-hidden="true"></i>`;
+        let abstractNode = document.createElement('tr');
+        abstractNode.innerHTML = `<td colspan="100">${curValues.abstract.replace(/\n/gm, "")}</td>`
+        x.parentNode.parentNode.insertBefore(abstractNode, x.parentNode.nextSibling);
+    }
+    // paper.values(curValues);
+    updateHighlights();
+}
